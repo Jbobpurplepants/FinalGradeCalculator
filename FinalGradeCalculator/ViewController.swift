c2272bc6612a11a49938a19a6e103243e62e6afc
Jacob Phipps <houseofphipps@gmail.com>
2022-09-13 10:35:17 -0600

    Initial Commit

---
+++ FinalGradeCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalGradeCalculator
+//
+//  Created by Phipps, Jacob - Student on 9/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
